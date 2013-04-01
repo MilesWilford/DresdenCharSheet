80063a9df5072a950d443a5f6576304fc0dfa911
MilesWilford <miles.wilford@gmail.com>
2013-03-31 23:50:28 -0400

    Completed flat of all but fatept corner

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 $(document).ready(function() {
     $(document).keyup(function() {
-        $('.char_name').html($("input[name='character']").val());
-        $('.player_name').html($("input[name='player']").val());
-        $('.high_concept').html($("input[name='high_concept']").val());
-        $('.trouble').html($("input[name='trouble']").val());
+        var dupfields = [];
+        dupfields.push($('.char_name'));
+        dupfields.push($('.player_name'));
+        dupfields.push($('.high_concept'));
+        dupfields.push($('.trouble'));
+
+        var duptargets = [];
+        duptargets.push($("input[name='character']").val());
+        duptargets.push($("input[name='player']").val());
+        duptargets.push($("input[name='high_concept']").val());
+        duptargets.push($("input[name='trouble']").val());
+
+        if (dupfields.length == duptargets.length) {
+            for (x in dupfields) {
+                dupfields[x].html(duptargets[x]);
+            }
+        }
+
+        var refreshCostCount = 0;
+        $(".stunts input[name$='cost']").each(function() {
+            var inputValue = parseInt($(this).val());
+            if (!isNaN(inputValue)) {
+                refreshCostCount += parseInt($(this).val());
+            }
+        });
+        $("#tot_stunt_refresh").html(Math.abs(refreshCostCount));
     });
 });
