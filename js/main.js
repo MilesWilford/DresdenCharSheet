bb5f7383605f15f4c055676364ee5c32a9ed7bc0
MilesWilford <miles.wilford@gmail.com>
2013-03-31 20:17:27 -0400

    Progress on Character Sheet page

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
     $(document).keyup(function() {
         $('.char_name').html($("input[name='character']").val());
         $('.player_name').html($("input[name='player']").val());
+        $('.high_concept').html($("input[name='high_concept']").val());
+        $('.trouble').html($("input[name='trouble']").val());
     });
 });
