754821d4a58109d7a326344167e9223b4110d063
MilesWilford <miles.wilford@gmail.com>
2013-04-01 12:10:55 -0400

    Added fate tracker early draft, repaired bad firefox layout and added minimal IE support

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
-    $(document).keyup(function() {
+    /* Changing the powerLevel triggers a few other fields to change */
+    $("select[name='powerlevel']").change(function() {
+        var selectedVal = parseInt($('option:selected', this).attr('value'));
+        var skillCap, skillPoints;
+        switch (selectedVal) {
+            case 6:
+                skillCap = 'Great (+4)';
+                skillPoints = '20';
+                break;
+            case 7:
+                skillCap = 'Great (+4)';
+                skillPoints = '25';
+                break;
+            case 8:
+                skillCap = 'Superb (+5)';
+                skillPoints = '30';
+                break;
+            case 10:
+                skillCap = 'Superb (+5)';
+                skillPoints = '35';
+                break;
+        }
+        $('#skill_cap').html(skillCap);
+        $('#skill_points').html(skillPoints);
+        $('#base_refresh').html(selectedVal);
+    });
+
+    /* Detects changes in all text fields which get copied to other fields */
+    $("input").change(function() {
         var dupfields = [];
         dupfields.push($('.char_name'));
         dupfields.push($('.player_name'));
@@ -20,6 +48,7 @@ $(document).ready(function() {
             }
         }
 
+        /* Sums up refresh cost counts of powers & stunts in bottom field */
         var refreshCostCount = 0;
         $(".stunts input[name$='cost']").each(function() {
             var inputValue = parseInt($(this).val());
