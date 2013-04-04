6b008017ae7f39bc673a7412e5cd3e95a639ba3b
MilesWilford <miles.wilford@gmail.com>
2013-04-04 18:54:41 -0400

    Fixed typo.

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ $(document).ready(function() {
             inputValueOf('cust_skill_cap'),
             inputValueOf('cust_total_skills'),
             inputValueOf('cust_base_refresh')
-        +);
+        );
     });
 
     // For the purpose of filling default values, simulate a change
